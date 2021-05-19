bca07e0dea5fa206283bca5c9bedb1d76b2df5ae
sainathphad <phadsainath1@gmail.com>
2021-05-19 22:33:30 +0530

    ADDED: Functionality to Calculate Length of Line

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class LineComparison {
     public static void main(String[] args) {
     System.out.println("Welcome To Line Comparison Computation Program");
+        int x1 = 2;
+        int x2 = 5;
+        int y1 = 2;
+        int y2 = 5;
+        double length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length of a line : " +length);
     }
 }
