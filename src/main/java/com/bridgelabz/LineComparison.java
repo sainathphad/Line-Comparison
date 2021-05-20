79e1376e562eb98d582567b92a061693c415cd74
sainathphad <phadsainath1@gmail.com>
2021-05-20 07:35:21 +0530

    Refactor: TO Compare Two Lines

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -3,10 +3,7 @@ package com.bridgelabz;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Computation Program");
-        int x1 = 2;
-        int x2 = 5;
-        int y1 = 2;
-        int y2 = 5;
+        int x1 = 2, x2 = 5, y1 = 2, y2 = 5;
         int p1 = 1, p2 = 5, q1 = 2, q2 = 5;
         double lengthOfLine1;
         double lengthOfLine2;
@@ -14,18 +11,9 @@ public class LineComparison {
         lengthOfLine2 = Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
         System.out.println("Length of line1 : " + lengthOfLine1);
         System.out.println("Length of Line 2: " + lengthOfLine2);
-       double result = Double.compare(lengthOfLine1, lengthOfLine2);
-        if (result == 0) {
-            System.out.println("line1 and line2 are equal");
-        }
-        else if (result < 0) {
-            System.out.println("line1 is shorter than line2");
-        }
-        else if (result > 0){
-            System.out.println("line1 is greater than line2");
-        }
-        else{
-            System.out.println("line1 and line2 are not equal");
-        }
+        Double d1 = lengthOfLine1;
+        Double d2 = lengthOfLine2;
+        System.out.println(d1.equals(d2));
+        System.out.println(d1.compareTo(d2));
     }
 }
