a2540d22645b598b9d9d216bc69f6700360cad91
sainathphad <phadsainath1@gmail.com>
2021-05-24 13:51:46 +0530

    REFACTORED: Used java oop concept and naming convention

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -1,22 +1,28 @@
 package com.bridgelabz;
 
 public class LineComparison {
-    static double LengthCalculation( int x1, int x2 , int y1, int y2 ) {
+
+    public static double lengthCalculation( int x1, int x2 , int y1, int y2 ) {
         return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
 
     public static void CompareLine() {
+        //VARIABLES
+        Double lengthOfLine1;
+        Double lengthOfLine2;
 
-        //COMPUTATION
-        Double lengthOfLine1 = LengthCalculation(2,5,2,5);
-        Double lengthOfLine2 = LengthCalculation(1,3,1,3);
+        //LENGTH COMPUTATION
+        lengthOfLine1 = lengthCalculation(2,5,2,5);
+        lengthOfLine2 = lengthCalculation(1,3,1,3);
 
         System.out.println("Length of line1 : " + lengthOfLine1);
         System.out.println("Length of Line2 : " + lengthOfLine2);
 
+        //EQUAL CHECK
         boolean checkLength = lengthOfLine1.equals(lengthOfLine2); //returns true or false
         System.out.println("Lines are equal: " + checkLength);
 
+        //COMPARE LINES
         int compareLine = lengthOfLine1.compareTo(lengthOfLine2);
         switch (compareLine) {
             case 0:
